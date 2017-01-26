cb6e004351585b98aaa198c5008db621d96241c4
Zafira Candra <zafiracandrawulan@gmail.com>
2017-01-26 21:20:58 +0700

    asp mvc 2

---
+++ asp_mvc_2/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;   
+using System.Web.Security;   
+using asp_mvc_2.Models.ViewModel;   
+using asp_mvc_2.Models.EntityManager; 
+ 
+namespace asp_mvc_2.Controllers   
+{ 
+    public class AccountController : Controller 
+    {       
+        public ActionResult SignUp() { 
+            return View(); 
+        } 
+ 
+        [HttpPost]
+        public ActionResult SignUp(UserSignUpView USV)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                if (!UM.IsLoginNameExist(USV.LoginName))
+                {
+                    UM.AddUserAccount(USV);
+                    FormsAuthentication.SetAuthCookie(USV.FirstName, false);
+                    return RedirectToAction("Welcome", "Home");
+
+                }
+                else
+                    ModelState.AddModelError("", "Login Name already taken.");
+            }
+            return View();
+        }
+    }
+}
---
+++ asp_mvc_2/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc; 
+ 
+namespace asp_mvc_2.Controllers   
+{ 
+    public class HomeController : Controller 
+    { 
+        public ActionResult Index() {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
